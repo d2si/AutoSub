094b90f7a2c2eda438b287dce56050c6019f7a97
Tony PHE <tony.phe@d2-si.eu>
2017-11-06 16:53:01 +0100

    terraform fmt + transcoding treatment: KO for the moment

--- infra/deploy/main.tf
+++ infra/deploy/main.tf
@@ -5,9 +5,9 @@
 #
 # TRANSCODER ROLE
 resource "aws_iam_role" "iam_role_transcoder" {
-    name = "transcoder_role"
+  name = "transcoder_role"
 
-assume_role_policy  = <<EOF
+  assume_role_policy = <<EOF
 {
     "Version": "2012-10-17",
     "Statement": [
@@ -24,10 +24,10 @@ EOF
 }
 
 resource "aws_iam_role_policy" "iam_policy_transcoder" {
-    name    = "transcoder_policy"
-    role    = "${aws_iam_role.iam_role_transcoder.name}"
+  name = "transcoder_policy"
+  role = "${aws_iam_role.iam_role_transcoder.name}"
 
-policy  = <<EOF
+  policy = <<EOF
 {
     "Version": "2012-10-17",
     "Statement": [
@@ -45,9 +45,9 @@ EOF
 
 # OUTPUT ROLE
 resource "aws_iam_role" "iam_role_lambda_speech" {
-    name                = "lambda_role_speech"
+  name = "lambda_role_speech"
 
-assume_role_policy  = <<EOF
+  assume_role_policy = <<EOF
 {
     "Version": "2012-10-17",
     "Statement": [
@@ -64,10 +64,10 @@ EOF
 }
 
 resource "aws_iam_role_policy" "iam_policy_lambda_speech" {
-    name    = "lambda_policy_speech"
-    role    = "${aws_iam_role.iam_role_lambda_speech.name}"
+  name = "lambda_policy_speech"
+  role = "${aws_iam_role.iam_role_lambda_speech.name}"
 
-policy  = <<EOF
+  policy = <<EOF
 {
     "Version": "2012-10-17",
     "Statement": [
@@ -83,25 +83,23 @@ policy  = <<EOF
 EOF
 }
 
-
 #
 # AWS S3 BUCKET CREATION
 #
 resource "aws_s3_bucket" "main" {
-    bucket = "autosub"
-    acl    = "private"
+  bucket = "autosub"
+  acl    = "private"
 
-    tags {
-        Name        = "autosub"
-        Environment = "Dev"
-    }
+  tags {
+    Name        = "autosub"
+    Environment = "Dev"
+  }
 }
 
-
 resource "aws_sns_topic" "input_notification" {
-    name = "s3-input-notification"
+  name = "s3-input-notification"
 
-policy = <<POLICY
+  policy = <<POLICY
 {
     "Version": "2012-10-17",
     "Statement": [
@@ -138,55 +136,54 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
 # AWS CREATE INPUT & OUTPUT FOLDERS
 #
 resource "aws_s3_bucket_object" "input_folder" {
-    bucket = "${aws_s3_bucket.main.id}"
-    acl    = "private"
-    key    = "Input/"
-    source = "/dev/null"
+  bucket = "${aws_s3_bucket.main.id}"
+  acl    = "private"
+  key    = "Input/"
+  source = "/dev/null"
 }
 
 resource "aws_s3_bucket_object" "output_folder" {
-    bucket = "${aws_s3_bucket.main.id}"
-    acl    = "private"
-    key    = "Output/"
-    source = "/dev/null"
+  bucket = "${aws_s3_bucket.main.id}"
+  acl    = "private"
+  key    = "Output/"
+  source = "/dev/null"
 }
 
 #
 # AWS ELASTIC TRANSCODER
 #
 resource "aws_elastictranscoder_pipeline" "transcoder_pipeline" {
-    input_bucket    = "${aws_s3_bucket.main.bucket}"
-    name            = "aws_etranscoder_pipeline_tf_autosub"
-    role            = "${aws_iam_role.lambda_extractor_role.arn}"
+  input_bucket = "${aws_s3_bucket.main.bucket}"
+  name         = "autosub-pipeline"
+  role         = "${aws_iam_role.lambda_extractor_role.arn}"
 
-    content_config {
-        bucket          = "${aws_s3_bucket.main.bucket}"
-        storage_class   = "Standard"
-    }
+  content_config {
+    bucket        = "${aws_s3_bucket.main.bucket}"
+    storage_class = "Standard"
+  }
 
-    thumbnail_config {
-        bucket          = "${aws_s3_bucket.main.bucket}"
-        storage_class   = "Standard"
-    }
+  thumbnail_config {
+    bucket        = "${aws_s3_bucket.main.bucket}"
+    storage_class = "Standard"
+  }
 }
 
 resource "aws_elastictranscoder_preset" "transcoder_preset" {
-    container   = "flac"
-    description = "autosub_preset"
-    name        = "autosub_preset"
+  container   = "flac"
+  description = "Preset for Autosub"
+  name        = "autosub-preset"
 
-    audio = {
-        audio_packing_mode = "SingleTrack"
-        channels           = 2
-        codec              = "flac"
-        sample_rate        = 44100
-    }
+  audio = {
+    audio_packing_mode = "SingleTrack"
+    channels           = 2
+    codec              = "flac"
+    sample_rate        = 44100
+  }
 
-    audio_codec_options = {
-        bit_depth   = 24
-    }
+  audio_codec_options = {
+    bit_depth = 24
+  }
 }
-
 
 ######################## GCP ########################
 
@@ -205,5 +202,4 @@ resource "aws_elastictranscoder_preset" "transcoder_preset" {
 #    name        = "autosub-gcp-bucket"
 #    location    = "EU"
 #}
-
 
