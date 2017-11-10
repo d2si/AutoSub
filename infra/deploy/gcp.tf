358d489032914514f5cfeebdda27f3d086c783ac
Ninir <gauthier.wallet@gmail.com>
2017-11-10 12:21:47 +0100

    Added Service Account + Policy

--- infra/deploy/gcp.tf
+++ infra/deploy/gcp.tf
@@ -7,3 +7,23 @@ resource "google_storage_bucket_acl" "main" {
   bucket         = "${google_storage_bucket.main.name}"
   predefined_acl = "private"
 }
+
+data "google_iam_policy" "admin" {
+  binding {
+    role = "roles/storage.admin"
+
+    members = [
+      "serviceAccount:${google_service_account.admin.email}",
+    ]
+  }
+}
+
+resource "google_service_account" "admin" {
+  account_id   = "${terraform.workspace}-autosub"
+  display_name = "${terraform.workspace}-autosub"
+}
+
+resource "google_project_iam_policy" "admin" {
+  project     = "${var.gcloud_project_id}"
+  policy_data = "${data.google_iam_policy.admin.policy_data}"
+}
